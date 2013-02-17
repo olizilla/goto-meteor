457a8e4c287476607281701053b1e29283b26cdd
olizilla <oli@zilla.org.uk>
2013-02-17 22:37:42 +0000

    Users gravatars now appear on the map

--- README.md
+++ README.md
@@ -4,9 +4,7 @@ README
 Put client resources like css and js libs into the client dir.
 
 
-
-Meteor for the impatient
-------------------------
+## Meteor for the impatient
 
 you can dump additional css files into the project and they automagically appear in the head in the rendered html
 they are added in alphabetical order, and files in subdirectories are added before files in in the root. The deeper in the tree the earlier it loads.
@@ -16,3 +14,6 @@ so for example we could be super luche and create a libs dir for all third party
 Put CSS & JS resources for the client in a dir called client. If you don't then your js libs will be processed on the server where there is no window object and cause an error. If you do, then all your libs and css will be automagically included in the head of your rendered html. The resrouces are automagically served with version info and cache headers for you dont have to think.
 
 
+## Gotchas that got me
+
+- Template methods used in the template, like grabbing the src for a gravatar url based on the current user, are called before 
--- client/client.js
+++ client/client.js
@@ -1,62 +1,173 @@
 /**
- * Every session gets a session id, and a tile on the board.
+ * Every session gets a session id
  * Mutiple sessions can have the same email address
+ *
+ * Show all users position on the map as avatar.
  */
 
 Meteor.startup(function () {
-  console.log('startup');
 
-  var retrieveOfCreatePlayerId = function(){
+	var retrieveOfCreatePlayerId = function(){
 
-    var playerId = window.localStorage['playerId'];
+		var playerId = window.localStorage['playerId'];
 
-    if (!playerId){
-      playerId = Players.insert({email: null});
-      window.localStorage['playerId'] = playerId;
-    }
+		// You're not from round here...
+		if (!playerId){
+			playerId = Players.insert({});
+			window.localStorage['playerId'] = playerId;
+		}
 
-    Session.set('playerId', playerId);
-  };
+		console.log('playerId: ', playerId);
 
-  // Do it.
-  retrieveOfCreatePlayerId();
-  
-  Session.set('greeting', 'OH HAI');
+		Session.set('playerId', playerId);
+
+		console.log('startup player:', getCurrentUser());
+	};
+
+	// Do it.
+	retrieveOfCreatePlayerId();
+
+	initMap();
+
+	getCurrentPosition();
+
+	Meteor.autorun(function(){
+//	var longitude = Session.get('longitude');
+//	var latitude = Session.get('latitude');
+//	var zoom = Session.get('zoom');
+
+		// console.log('startup autorun called', player);
+
+		Players.find().forEach(function(player){
+			if (!player || !player.position || !map){
+			return false;
+			}
+			var longitude = player.position.coords.longitude;
+			var latitude = player.position.coords.latitude;
+			var zoom = player.position.zoom;
+
+			var pos = lonlat(longitude, latitude);
+
+			// map.panTo(point);
+			// map.zoomTo(zoom);
+
+			var pointFeature = createPoint(pos);
+			pointFeature.attributes.gravatar = gravatarUrl(player.emailHash);
+
+			console.log(positionsLayer, pointFeature);
+			positionsLayer.addFeatures( [pointFeature] );
+		});
+
+		// var player = Players.findOne(Session.get('playerId'));
+
+	});
 });
 
-Template.hello.greeting = function () {
-  return Session.get('greeting');
+Template.gravatar.url = function(){
+	var player = Players.findOne(Session.get('playerId'));
+	
+	console.log('Template gravatar.url called', player);
+	
+	if (!player || !player.emailHash){
+		return false;// don't return null, always return a default or you get errors.
+	}
+
+	return gravatarUrl(player.emailHash);
 };
 
-Template.hello.avatarUrl = function(){
-  var player = Players.findOne(Session.get('playerId'));
-  if (!player){
-    return "placeholder.png"; // don't return null, always return a default or you get errors.
-  }
-  var email = player.email;
-  return toAvatarUrl(email);
-};
+Template.gravatar.events({
 
-Template.hello.events({
+	'click .save' : function (event, template) {
 
-  'click .save' : function (event, template) {
-    
-    var email = template.find('.email').value;
+		var email = template.find('.email').value;
 
-    if (email && email !== ''){
-      Players.update(Session.get('playerId'), { email: email });  
-    }
+		if (email && email !== ''){
+			var hash = $.md5(email);
+			Players.update(Session.get('playerId'), {$set: { emailHash: hash }});
+		}
 
-    console.log(Players.findOne(Session.get('playerId')));
-  }
-  
+		console.log(Players.findOne(Session.get('playerId')));
+	}
 });
 
 function getCurrentUser() {
-  return Players.findOne(Session.get('playerId'));
+	return Players.findOne(Session.get('playerId'));
 }
 
-function toAvatarUrl(email) {
-  hash = $.md5(email);
-  return 'http://www.gravatar.com/avatar/' + hash;
+function gravatarUrl(hash) {
+	return 'http://www.gravatar.com/avatar/' + hash;
+}
+
+var map;
+var positionsLayer;
+
+function initMap() {
+
+	map = new OpenLayers.Map({});
+
+	var gmap = new OpenLayers.Layer.Google("Google Streets");
+
+	var ships = new OpenLayers.Layer.Vector();
+
+	map.addLayers([gmap]);
+
+	map.zoomToMaxExtent();
+
+	// map.panTo(lonlat(-40, 46));
+
+	map.render('map');
+
+	addVectorLayer('peas');
+
+	return map;
+}
+
+function lonlat(lon, lat) {
+
+	var epsg4326 = new OpenLayers.Projection("EPSG:4326");
+
+	var epsg900913 = new OpenLayers.Projection("EPSG:900913");
+
+	var point =  new OpenLayers.LonLat(lon, lat).transform(epsg4326, epsg900913);
+
+	return point;
+}
+
+function getCurrentPosition(){
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(pos){
+
+			var clonePos = $.extend(true, {}, pos); // Fix FF error 'Cannot modify properties of a WrappedNative'
+			clonePos.zoom = 11;
+			Players.update(Session.get('playerId'), {$set: { position: clonePos }});
+
+		}, error);
+	} else {
+		error('not supported');
+	}
+}
+
+function addVectorLayer(name){
+
+	positionsLayer = new OpenLayers.Layer.Vector(name, {
+		projection: new OpenLayers.Projection('EPSG:4326'),
+		styleMap: new OpenLayers.StyleMap({
+			graphicWidth: 40,
+			graphicHeight: 40,
+			externalGraphic: '${gravatar}'
+		})
+	});
+
+	map.addLayer(positionsLayer);
+}
+
+function createPoint(coords){
+
+	var point = new OpenLayers.Geometry.Point(coords.lon, coords.lat);
+    var pointFeature = new OpenLayers.Feature.Vector(point);
+    return pointFeature;
+}
+
+function error(msg) {
+	console.log(arguments);
 }
